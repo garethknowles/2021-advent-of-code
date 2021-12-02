9115b129689a75ba69d1d3ea2f069a9f5a0117ba
Gareth Knowles <gareth@mettle.co.uk>
2021-12-02 16:46:35 +0000

    Better day setup with some boilerplate

--- setup-day.sh
+++ setup-day.sh
@@ -11,10 +11,27 @@ echo "#!/usr/bin/env python3
 
 with open((__file__.rstrip(\"solution.py\")+\"input.txt\"), 'r') as input_file:
     input = input_file.read()
+data = [str(line) for line in input.splitlines()]
+test_data = [
 
-output = [str(line) for line in input.splitlines()]
+]
 
-print(\"Part 1 Output:\" + str(output))
+def part1(input):
+    return 0
+
+def part2(input):
+    return 0
+
+print(\"Part 1 Test Output:\")
+print(str(part1(test_data)))
+print(\"Part 1 Output:\")
+print(str(part1(data)))
+
+print(\"Part 2 Test Output:\")
+print(str(part2(test_data)))
+print(\"Part 2 Output:\")
+print(str(part2(data)))
+
 " >> solution.py
 
 chmod +x solution.py
